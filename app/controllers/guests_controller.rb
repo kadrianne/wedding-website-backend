7c2ebada8368e647f7bd463a418f33fad290ad99
kadrianne <kristine.a.du@gmail.com>
2020-04-07 12:21:51 -0600

    Create action for guests controller.

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -5,4 +5,20 @@ class GuestsController < ApplicationController
         render json: @guests, include: [:address => {except: [:created_at, :updated_at]}, :household => {except: [:created_at, :updated_at]}], except: [:created_at, :updated_at, :address_id, :household_id]
     end
     
+    def create
+        @guest = Guest.new(guest_params)
+
+        if @guest.save
+            render json: {message: 'Guest successfully added.', guest: @guest}, status: :created
+        else
+            render @guest.errors.messages
+        end
+
+    end
+
+    private
+
+    def guest_params
+        params.require(:guest).permit([:first_name,:last_name,:age,:email,:phone,:rsvp,:household_id,:address_id])
+    end
 end
--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,5 +1,6 @@
 class Guest < ApplicationRecord
   belongs_to :household
   belongs_to :address
+  validates :first_name,:last_name,:email,:age, presence: true
   
 end
