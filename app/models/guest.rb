2026bc5e42d5900c0cce7493fc99997599e9bbb5
kadrianne <kristine.a.du@gmail.com>
2020-04-15 16:55:11 -0600

    Make address optional for guests.

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,6 +1,6 @@
 class Guest < ApplicationRecord
   belongs_to :household
-  belongs_to :address
+  belongs_to :address, optional: true
   validates :first_name, :last_name, :household, presence: true
   
 end
--- db/migrate/20200415223112_add_address_to_guests.rb
+++ db/migrate/20200415223112_add_address_to_guests.rb
@@ -1,5 +1,5 @@
 class AddAddressToGuests < ActiveRecord::Migration[6.0]
   def change
-    add_reference :guests, :address
+    add_reference :guests, :address, null: true
   end
 end
